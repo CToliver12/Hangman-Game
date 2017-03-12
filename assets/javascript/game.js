6ea3b64dcaf11c5631741427b0c6473a4c2847a8
Candace <seekaye@gmail.com>
2017-03-12 06:28:57 -0700

    hangman

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,133 +1,128 @@
-//Variables
-	var wins = 0;
-	var placeholderArray = []
-	var prevPlaceholderArray = [];
-	var wordPlaceholder = [];
-	var lettersGuessed =[];
-	var word = [];
-	var wordPlaceholderString = "";
-	var userInput= "";
-	var correctGuessCount = 0;
-	var guessesLeft = 15;
+//VARIABLES
+//=====================================================
+//Arrays and Variables for holding data 
+var wordOptions = ["butterflies", "thriller", "ben", "dirtydiana", "bad", "beatit", "dangerous"];
+var selectedWord = "";
+var lettersinWord = [];
+var numBlanks = 0; 
+var blanksAndSuccesses = []; // b _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+var wrongLetters = [];
 
-	// Object of 90's Groups Hangman Words. 
-	var ninetysGroupsWords = {
-		word1: ["T", "U","P","A","C"],
-		word2: ["S","N","O","O","P","D","O","G","G"],
-		word3: ["C","O","M","M","O","N"], 
-		word4: ["A","A","L","I","Y","A","H"],
-		word5: ["B","A","C","K","S","T","R","E","E","T","B","O","Y","S"],
-		word6: ["N","O","D","O","U","B","T"],
-		word7: ["B","I","G","G","I","E"],
-	};
+//Game counters 
+var winCount = 0; 
+var lossCount = 0;
+var guessesLeft = 9;
 
-//Array of 90s Hangman words created from object.
+//FUNCTIONS 
+//===================================================
+function startGame () {
+	selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
+	lettersinWord = selectedWord.split("");
+	numBlanks = lettersinWord.length;
 
-var wordArray = [ninetysGroupsWords.word1, ninetysGroupsWords.word2, ninetysGroupsWords.word3, ninetysGroupsWords.word4, ninetysGroupsWords.word5, ninetysGroupsWords.word6, ninetysGroupsWords.word7];
-function createWord(wordArray){
+	//Reset
+	guessesLeft = 9; 
+ 	wrongLetters =[];
+ 	blanksAndSuccesses = [];
 
-//Create array from randomly selected word Array
-word = wordArray[Math.floor(Math.random()*wordArray.length)];
-	console.log(word);
+ 	//Populate blanks and success with right number of blanks using FOR LOOP.
+ 	for (var i=0; i<numBlanks; i++){
+ 		blanksAndSuccesses.push("_");
+ 	}
 
-//Create placeholder for word
-createWordPlaceholder(word);
-return word;
-};
+ 	//HTML changing to reflect conditions of game....using innerHTML
+	document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join(" ");
+	document.getElementById("numGuesses").innerHTML = guessesLeft;
+	document.getElementById("winCounter").innerHTML = winCount;
+	document.getElementById("lossCounter").innerHTML = lossCount;
 
-//Create placeholder for word
-function createWordPlaceholder (word) {
-	var wordPlaceholder = [];
+ 	//Testing / Debugging 
+	console.log(selectedWord);
+	console.log(lettersinWord); 
+	console.log(numBlanks);
+	console.log(blanksAndSuccesses); 
+} 
 
-//Fill array with underscores. 
-for (var i = 0; i < word.length; i++){
-	wordPlaceholder.push("_"); 
-}
+function checkletters(letter){
+	//Check if letter exists in code at all 
 
-//Convert word placeholder array to string for displaying
-wordPlaceholderString = wordPlaceholder.join (" ");
+	var isLetterInWord = false; 
 
-//Display word placeholder
-document.getElementById ('word-placeholder').textContent = wordPlaceholderString;
-return wordPlaceholder;
+	for(var i=0; i<numBlanks; i++){
+		if(selectedWord[i] == letter){
+			isLetterInWord = true;
 
-};
-
-function trackLetterGuesses(userInput) {
-
-	/*
-	*Check if letter already guessed. 
-	*Don't track letters more than once. 
-	*/
-	for (var i=0; i < lettersGuessed.length; i++){
-		if(userInput == lettersGuessed[i]){
-			return;
 		}
 	}
 
-//Push letter guessed. 
-lettersGuessed.push(userInput);
-console.log("lettersGuessed array item:" + lettersGuessed);
-
-//Convert letters guessed array to string for displaying
-var lettersGuessedString = lettersGuessed.join (", ");
-document.getElementById('letters-guessed').innerHTML = lettersGuessedString;
-
-//Each guess reduces number of guesses left.  
-guessesLeft--;
-
-//Display guesses left. 
-document.getElementById('guess-count').innerHTML = guessesLeft;
-console.log ('Guesses left' + guessesLeft);
-
-//Game restarts when no mre guesses left. 
-if(guessesLeft == 0) {
-	restartGame();
+//Check where in word letter exists, then populate out blanksAndSuccesses array. 
+if(isLetterInWord){
+	for(var i=0; i<numBlanks; i++){
+		if(selectedWord[i] == letter){
+			blanksAndSuccesses[i] = letter;
+		}
+	}
 }
-return lettersGuessedString;
 
-};
+else {
+	wrongLetters.push(letter);
+	guessesLeft--
+}
 
+console.log(blanksAndSuccesses);
+}
 
-createWord(wordArray);
-//Initialize game on window load.
+function roundComplete(){
+	console.log("Win Count:" + winCount + " | Loss Count: " + lossCount + "| Guesses Left" + guessesLeft);
 
-//Main Process
-document.onkeyup = function(event){
-	console.log ('This is the Key entered', event.key);
-	var keyPress;
+	//Update the HTML to reflect the most recent count stats 
+	document.getElementById("numGuesses").innerHTML = guessesLeft;
+	document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join(" ");
+	document.getElementById("wrongGuesses").innerHTML = wrongLetters.join(" ");
 
-if (typeof event != 'undefined') {
-	keyPress = event.keyCode;
+//Check if user won 
+if(lettersinWord.toString() == blanksAndSuccesses.toString()){
+	winCount++;
+	alert("You Won!");
 
-//Convert user input key to upper case string.
-//userInput =String.fromCharCode(keyPress).toUpperCase();
-//console.log(userInput + " should match the key entered");
+//Update the win counter in the HTML
+document.getElementById("winCounter").innerHTML = winCount;
 
-//Track user guesses over time.
-trackLetterGuesses(userInput);
+startGame();
 
-//Build hangman word based on new user input.
-// buildWord(userInput);
-};
+}
 
+//Check if user lost
+else if (guessesLeft == 0){
+	lossCount++;
+	alert("You Lost!");
 
+	//Update the HTML
+	document.getElementById("lossCounter").innerHTML = lossCount;
 
-
-
-//FUNCTIONS
-//=============================
-
-
-//Array created from randomly selected 90s word Array.
-
-
-//Create placeholder for word 
-
-
-//Keep track of user guesses. 
-
-};
+	startGame();
+}
+}
 
 
-//Builds hangman word as user guesses letters.
+	
+	
+
+
+
+	//Main Process
+	//==================================================
+	
+	//Initiates the code the first time
+	startGame();
+
+//Register keyclicks
+document.onkeyup = function(event) {
+	var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+	checkletters(letterGuessed);
+	roundComplete();
+
+	//Testing/Debugging
+	console.log(letterGuessed);
+}
+
